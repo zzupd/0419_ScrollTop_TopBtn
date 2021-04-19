6347e705ab11c5888ae9aa869fa5609a6462fa9e
zzupd <zzupd@naver.com>
2021-04-19 15:32:15 +0900

    0419_스크롤탑버튼_Fade효과

--- script/script.js
+++ script/script.js
@@ -6,4 +6,18 @@ $(function(){
         $(window).scrollTop(0);
     });
 
+    $(window).scroll(function(){
+        // 화면전체가 스크롤되는 것은 window를
+        // 선택자로 지정함
+        //alert($(this).scrollTop() + " px");
+
+        if ($(this).scrollTop() > 300) {
+            $("div#topBtnArea").fadeIn(200);
+        } else {
+            $("div#topBtnArea").fadeOut(200);
+        }
+
+
+    });
+
 });
